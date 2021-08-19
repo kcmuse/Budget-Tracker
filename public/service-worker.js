fe6f0380af5b958f0076254e7ade49dfb4fd86fb
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-08-19 10:56:44 -0700

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,8 +6,8 @@ const FILES_TO_CACHE = [
   '/db.js',
   '/manifest.webmanifest',
   '/styles.css',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
 ];
 
 self.addEventListener("install", function (evt) {
