1bf97644929477e94fdb370c7c4dda19e6c651b2
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-08-19 11:00:26 -0700

    new update

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,7 +12,9 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", function (evt) {
     // pre cache image data
-    
+    evt.waitUntil(
+      caches.open(DATA_CACHE_NAME).then((cache) => cache.add('/api/transactions'))
+    );
     // pre cache all static assets
     evt.waitUntil(
       caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
